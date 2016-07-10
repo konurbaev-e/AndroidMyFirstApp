51812483aa6637cd00c682be66f880dfd39767ea
konurbaev-e <forget>
2016-07-10 23:18:29 +0300

    second commit

--- app/src/main/java/org/konurbaev/myfirstapp/MyActivity.java
+++ app/src/main/java/org/konurbaev/myfirstapp/MyActivity.java
@@ -1,7 +1,7 @@
 package org.konurbaev.myfirstapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MyActivity extends AppCompatActivity {
 
